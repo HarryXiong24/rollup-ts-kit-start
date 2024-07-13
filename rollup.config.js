61d8fb9130ebd3a1dcfb6ccab6b938a86e367df6
Harry Xiong <harryxiong24@gmail.com>
2024-07-13 00:15:17 -0700

    feat: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,37 @@
 // rollup.config.js
-import babel from 'rollup-plugin-babel';
-import noderesolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import aliasPlugin from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import { uglify } from 'rollup-plugin-uglify';
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-const plugins = isProduction ? [uglify()] : undefined;
+const plugins = isProduction ? [uglify()] : [];
+const commonAliases = [];
+const config = {
+  input: './src/main',
+  output: './lib/bundle',
+};
 
 export default {
-  input: './src/main.ts', // 入口文件
+  input: `${config.input}.ts`, // 入口文件
   output: [
     {
-      file: './lib/bundle.cjs.js', // 打包后的存放文件
+      file: `${config.output}.cjs.js`, // 打包后的存放文件
       format: 'cjs', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
       name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
       sourcemap: true, // 生成 bundle.map.js 文件，方便调试
     },
     {
-      file: './lib/bundle.es.js', // 打包后的存放文件
+      file: `${config.output}.es.js`, // 打包后的存放文件
       format: 'es', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
       name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
       sourcemap: true, // 生成 bundle.map.js 文件，方便调试
     },
     {
-      file: './lib/bundle.umd.js', // 打包后的存放文件
+      file: `${config.output}.umd.js`, // 打包后的存放文件
       format: 'umd', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
       name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
       sourcemap: true, // 生成 bundle.map.js 文件，方便调试
@@ -37,11 +42,10 @@ export default {
   },
   plugins: [
     typescript(),
-    noderesolve(),
+    nodeResolve(),
+    json(),
     commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
+    aliasPlugin({ entries: commonAliases }),
     terser(),
     ...plugins,
   ],
